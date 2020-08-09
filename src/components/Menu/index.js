dfc7e205cc2d7464417247b865636194320de65a
Gustavo-A-Mendes <63437313+Gustavo-A-Mendes@users.noreply.github.com>
2020-08-09 08:29:43 -0300

    ESlint fix

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
 //  import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-      <nav className='Menu'>
-        <Link to='/'>
-          <img LinklassName='Logo' src={Logo} alt='Gustaflix logo'></img>
-        </Link>
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img LinklassName="Logo" src={Logo} alt="Gustaflix logo" />
+      </Link>
 
-        <Button as={Link} className='ButtonLink' to='/cadastro/video'>
-          Novo vídeo
-        </Button>
-      </nav>  
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
